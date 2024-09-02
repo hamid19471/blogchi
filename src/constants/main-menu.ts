5b0019f9ea0933221b6813aae260191e34e2f727
Hamid Asadi <hamid.19471@gmail.com>
2024-09-02 13:46:40 +0330

    Create: constant for main navigation and expose that with index file

---
+++ src/constants/main-menu.ts
@@ -0,0 +1,23 @@
+type MenuItems = {
+  name: string;
+  href: string;
+};
+
+export const MainMenu: MenuItems[] = [
+  {
+    name: "خانه",
+    href: "/",
+  },
+  {
+    name: "مقالات",
+    href: "/blog",
+  },
+  {
+    name: "درباره ما",
+    href: "/about",
+  },
+  {
+    name: "تماس با ما",
+    href: "/contact",
+  },
+];
