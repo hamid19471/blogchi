7332cb1691332a81aae5f533191bb7a36bce0009
Hamid Asadi <hamid.19471@gmail.com>
2024-09-03 15:01:30 +0330

    Update: import home hero section in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import { HomeHero } from "./_components/home-hero";
 
 export default function Home() {
-  return <div>وبلاگ چی</div>;
+  return (
+    <div>
+      <HomeHero />
+    </div>
+  );
 }
