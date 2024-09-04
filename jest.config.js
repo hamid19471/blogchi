c557316cfa9a76319218fb20df7e3d281c174eb2
Hamid Asadi <hamid.19471@gmail.com>
2024-09-05 02:00:42 +0330

    Update: config

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,7 @@ const config = {
   // Add more setup options before each test is run
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   preset: "ts-jest",
+  modulePathIgnorePatterns: ["node_module", "jest-results.json"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
