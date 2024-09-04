82fa0887f12b1bb4b91d45d29b7f7d1bd826df30
Hamid Asadi <hamid.19471@gmail.com>
2024-09-04 15:11:18 +0330

    Update: path and add more addons

---
+++ .storybook/main.ts
@@ -0,0 +1,22 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-controls",
+  ],
+  typescript: {
+    reactDocgen: "react-docgen-typescript", // Allows TypeScript control in Storybook
+  },
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  staticDirs: ["../public"],
+};
+export default config;
