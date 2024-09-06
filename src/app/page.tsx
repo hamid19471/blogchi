6d22ca4fcff98c85fde954542af31e3390ae07ea
Hamid Asadi <hamid.19471@gmail.com>
2024-09-06 13:49:31 +0330

    Update: fetch list of post and add blog post list in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
-import Image from "next/image";
+import { PostBlogs } from "@/types/post-blogs.interface";
 import { HomeHero } from "./_components/home-hero";
+import { BlogPostsList } from "./blog/components/blog-posts-list";
 
-export default function Home() {
+const getNewstBlot = async (): Promise<PostBlogs> => {
+  const res = await fetch(`http://localhost:4000/api/post/list`, {
+    next: {
+      revalidate: 40,
+    },
+  });
+  return res.json();
+};
+
+export default async function Home() {
+  const {
+    data: { posts },
+  } = await getNewstBlot();
+
   return (
-    <div>
+    <>
       <HomeHero />
-    </div>
+      <section className="container mt-20">
+        <div className="text-center xl:text-right">
+          <h2 className="text-3xl font-extrabold">جدیدترین مقالات</h2>
+          <p>جدیدترین اخبار و مقالات آموزشی در حوزه فن آوری اطلاعات</p>
+        </div>
+        <BlogPostsList posts={posts} />
+      </section>
+    </>
   );
 }
