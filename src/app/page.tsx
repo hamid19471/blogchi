2ab9676845060158c6429a8d3dcf61cdb10b0346
Hamid Asadi <hamid.19471@gmail.com>
2024-09-07 21:16:22 +0330

    Update: use env variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { PostBlogs } from "@/types/post-blogs.interface";
 import { HomeHero } from "./_components/home-hero";
 import { BlogPostsList } from "./blog/components/blog-posts-list";
+import { API_URL } from "@/configs/global";
+import { Button } from "./_components/button";
+import { ArrowLeft } from "lucide-react";
 
 const getNewstBlot = async (): Promise<PostBlogs> => {
-  const res = await fetch(`http://localhost:4000/api/post/list`, {
+  const res = await fetch(`${API_URL}/post/list`, {
     next: {
       revalidate: 40,
     },
@@ -15,14 +18,19 @@ export default async function Home() {
   const {
     data: { posts },
   } = await getNewstBlot();
-
   return (
     <>
       <HomeHero />
       <section className="container mt-20">
-        <div className="text-center xl:text-right">
-          <h2 className="text-3xl font-extrabold">جدیدترین مقالات</h2>
-          <p>جدیدترین اخبار و مقالات آموزشی در حوزه فن آوری اطلاعات</p>
+        <div className=" flex flex-col md:flex-row items-center justify-between gap-8 md:gap-2">
+          <div className="text-center xl:text-right">
+            <h2 className="text-3xl font-extrabold">جدیدترین مقالات</h2>
+            <p>جدیدترین اخبار و مقالات آموزشی در حوزه فن آوری اطلاعات</p>
+          </div>
+          <Button variant="neutral" size="large">
+            تمامی مقالات
+            <ArrowLeft size={14} />
+          </Button>
         </div>
         <BlogPostsList posts={posts} />
       </section>
