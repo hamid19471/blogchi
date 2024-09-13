3cf1165bdb7ce843e1d27d1119c9e7b45a8e946c
Hamid Asadi <hamid.19471@gmail.com>
2024-09-13 23:15:21 +0330

    Create: Subscribtion section and used in home page

---
+++ src/app/_components/subscribtion/subscribtion.tsx
@@ -0,0 +1,14 @@
+export const Subscribtion: React.FC = () => {
+  return (
+    <>
+      <div className="text-center flex items-center justify-center flex-col">
+        <h2 className="font-bold text-7xl gradiant mb-5">عضویت در خبرنامه</h2>
+        <p className="font-light text-sm">
+          با عضویت در خبرنامه از جدیدترین اخبار و مقالات آموزشی در حوزه فن آوری
+          اطلاعات باخبر شوید
+        </p>
+        <div className="mt-14">subscribtion form</div>
+      </div>
+    </>
+  );
+};
