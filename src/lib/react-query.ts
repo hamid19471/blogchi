1de93ad9eabba367fdeefbfff4e2991915ae1963
Hamid Asadi <hamid.19471@gmail.com>
2024-09-14 20:55:34 +0330

    Create: customized new query client

---
+++ src/lib/react-query.ts
@@ -0,0 +1,18 @@
+import { QueryCache, QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) => {
+      // norification Errors
+    },
+  }),
+
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+      gcTime: 1000 * 60 * 60 * 24,
+      throwOnError: false,
+    },
+  },
+});
