03b74e2413f5594b9523657e79fcdca529e77914
Hamid Asadi <hamid.19471@gmail.com>
2024-09-16 13:25:30 +0330

    Update: add signin and signup button in header component

--- src/app/_components/header/header.tsx
+++ src/app/_components/header/header.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import { MainNavigation } from "./main-navigation";
+import { Button } from "../button";
+import { User, UserPlus } from "lucide-react";
+import Link from "next/link";
 
 export const Header: React.FC = () => {
   return (
@@ -7,7 +10,18 @@ export const Header: React.FC = () => {
       <div className="container flex items-center justify-between">
         <Image src={"/images/logo.svg"} width={100} height={80} alt="بلاگ چی" />
         <MainNavigation />
-        <div className="mr-auto">user Athentication</div>
+        <div className="mr-auto">
+          <div className="flex items-center gap-x-2">
+            <Button variant="primary" size="normal">
+              <UserPlus size={18} />
+              <Link href={"/signup"}>ثبت نام</Link>
+            </Button>
+            <Button variant="secondary" size="normal">
+              <Link href={"/signin"}>ورود</Link>
+              <User size={18} />
+            </Button>
+          </div>
+        </div>
       </div>
     </header>
   );
