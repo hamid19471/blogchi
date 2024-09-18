1a019b720644c38aff1bd221b7a66e39f7dc9cab
Hamid Asadi <hamid.19471@gmail.com>
2024-09-19 00:41:38 +0330

    Update: add sigin and signup forms in auth routes

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,3 +1,9 @@
+import { SigninForm } from "./_components/signin-form";
+
 export default async function SigninPage() {
-  return <div>Signin Page</div>;
+  return (
+    <div className="w-2/3">
+      <SigninForm />
+    </div>
+  );
 }
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,3 +1,9 @@
+import { SigninForm } from "./_components/signup-form";
+
 export default async function SignupPage() {
-  return <div>Signup Page</div>;
+  return (
+    <div className="w-2/3">
+      <SigninForm />
+    </div>
+  );
 }
