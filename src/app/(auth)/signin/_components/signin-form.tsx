9d8abbcf79a4aff94da9129d05fee2b267ecb9b8
Hamid Asadi <hamid.19471@gmail.com>
2024-09-20 15:29:45 +0330

    Update: add react hook form to component

--- src/app/(auth)/signin/_components/signin-form.tsx
+++ src/app/(auth)/signin/_components/signin-form.tsx
@@ -3,16 +3,52 @@
 import { Button } from "@/app/_components/button";
 import { Textbox } from "@/app/_components/textbox";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { SigninFormValues } from "../_types/signin.interface";
 
 export const SigninForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SigninFormValues>({
+    mode: "all",
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data: SigninFormValues) => {
+    console.log(data);
+  };
+
   return (
     <>
       <h3 className="font-black text-3xl mb-2">ورود</h3>
       <p className="font-light mb-12">ورود به حساب کاربری</p>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="flex flex-col gap-4 mb-12">
-          <Textbox placeholder="پست الکترونیک" type="email" />
-          <Textbox placeholder="رمز عبور" type="password" />
+          <Textbox
+            {...register("email", {
+              required: {
+                value: true,
+                message: "ایمیل خود را وارد کنید",
+              },
+            })}
+            placeholder="پست الکترونیک"
+            type="email"
+          />
+          <Textbox
+            {...register("password", {
+              required: {
+                value: true,
+                message: "رمز عبور خود را وارد کنید",
+              },
+            })}
+            placeholder="رمز عبور"
+            type="password"
+          />
         </div>
         <div>
           <Button type="submit" variant="primary" className="w-full">
